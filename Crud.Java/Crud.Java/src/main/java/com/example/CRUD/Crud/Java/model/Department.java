5c9bef6db5b8ce134212a522c14fc12d11bae564
Jose Fabian <jose.fabian@consensus.com>
2022-04-25 16:52:59 -0400

    adding deparment

---
+++ Crud.Java/Crud.Java/src/main/java/com/example/CRUD/Crud/Java/model/Department.java
@@ -0,0 +1,22 @@
+package com.example.CRUD.Crud.Java.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+}
